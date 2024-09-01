db32e84b567c91d7590a52f777df6e229517eff6
manishshettym <manishs@berkeley.edu>
2024-09-01 15:22:14 -0700

    test: improve test program

--- test/test.c
+++ test/test.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void foo()
+void pointerOperations()
 {
-    printf("Hello, World!\n");
+    int arr[5] = {1, 2, 3, 4, 5};
+    int *ptr = arr;
+
+    // Perform pointer arithmetic
+    ptr += 3;
+    printf("Pointer Arithmetic Result: %d\n", *ptr);
+
+    int a = 10;
+    void *voidPtr = &a;
+
+    // Cast void pointer to int pointer and dereference
+    int *intPtr = (int *)voidPtr;
+    printf("Pointer Cast Result: %d\n", *intPtr);
+
+    int *rawPtr = (int *)malloc(sizeof(int));
+    *rawPtr = 20; // Dereference raw pointer to assign value
+
+    printf("Raw Pointer Dereference Result: %d\n", *rawPtr);
+    free(rawPtr);
 }
 
 int main()
 {
-    foo();
+    pointerOperations();
     return 0;
 }
